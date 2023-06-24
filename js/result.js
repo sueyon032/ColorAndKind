3673fe01372578f8c243e6671af06d0a65165797
JISOO <kwonjisoo14@gmail.com>
2023-06-24 20:58:13 +0900

    red, blue, green test기능 추가

--- js/result.js
+++ js/result.js
@@ -2,8 +2,17 @@ function result(mode){
     if(mode=="yes"){
         document.getElementById("resImg").src = "images/testRes_no.png";
     }
-	else if(mode=="no") {
+	else if(mode=="noAll") {
         document.getElementById("resImg").src = "images/testRes_all.png";
+    }
+	else if(mode=="noBlue") {
+        document.getElementById("resImg").src = "images/testRes_blue.png";
+    }
+	else if(mode=="noGreen") {
+        document.getElementById("resImg").src = "images/testRes_green.png";
+    }
+	else if(mode=="noRed") {
+        document.getElementById("resImg").src = "images/testRes_red.png";
     }
     else if(mode=="re") {
         document.getElementById("resImg").src = "images/testRes_yet.png";
